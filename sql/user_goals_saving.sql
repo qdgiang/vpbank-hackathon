3ba7a5a49901395f3218b957bb75385d2db84879
nguyenlechau <nguyenlechau.mfc@gmail.com>
2025-07-10 00:17:21 +0700

    Update user_goals_saving.sql

--- sql/user_goals_saving.sql
+++ sql/user_goals_saving.sql
@@ -1,20 +1,24 @@
-CREATE TABLE saving_goals
+CREATE TABLE goal_setting
 (
-    goal_id        CHAR(36)                 DEFAULT (UUID()) NOT NULL
+    goal_id             CHAR(36)                 DEFAULT (UUID())    NOT NULL
         PRIMARY KEY,
-    user_id        CHAR(36)                                  NOT NULL,
-    goal_name      VARCHAR(255)                              NOT NULL,
-    target_amount  DECIMAL(15, 2)                            NOT NULL,
-    current_amount DECIMAL(15, 2)           DEFAULT 0.00,
-    target_date    DATE,
-    goal_type      VARCHAR(20)              DEFAULT 'long_term',
-    priority_level INTEGER                  DEFAULT 1,
-    is_active      BOOLEAN                  DEFAULT TRUE,
-    created_at     TIMESTAMP                DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP                DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id             CHAR(36)                                     NOT NULL,
+    goal_name           VARCHAR(255)                                 NOT NULL,
+    target_amount       NUMERIC(15,2)                                NOT NULL,
+    current_amount      NUMERIC(15,2)            DEFAULT 0.00,
+    target_date         DATE                                         NOT NULL,
+    goal_type           VARCHAR(20),
+    priority_level      INTEGER                   CHECK (priority_level IN (1, 2, 3)),
+    weight              NUMERIC(5,2),
+    is_active           BOOLEAN                   DEFAULT TRUE,
+    eta_lock            BOOLEAN                   DEFAULT FALSE,
+    initial_target_date DATE,
+    sent_money          BOOLEAN                   DEFAULT FALSE,
+    created_at          TIMESTAMP                 DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP                 DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT valid_amounts 
         CHECK (target_amount > 0 AND current_amount >= 0),
-    CONSTRAINT fk_saving_goals_user 
+    CONSTRAINT fk_goal_setting_user 
         FOREIGN KEY (user_id) REFERENCES users(user_id)
         ON DELETE CASCADE
 );
