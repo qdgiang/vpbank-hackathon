db2cde39643feca9081f34f6e69a02c8b239df5c
nguyenlechau <nguyenlechau.mfc@gmail.com>
2025-07-17 02:16:10 +0700

    terraform goals update

--- terraform/lambda_crud_goal.tf
+++ terraform/lambda_crud_goal.tf
@@ -1,5 +1,5 @@
-# Lambda layer for crud_goal function
-resource "null_resource" "crud_goal_lambda_layer" {
+# --- Lambda Layer for Python packages ---
+resource "null_resource" "goal_lambda_layer" {
   triggers = {
     requirements = filemd5("${path.module}/../lambda/crud_goal/requirements.txt")
   }
@@ -13,50 +13,49 @@ resource "null_resource" "crud_goal_lambda_layer" {
   }
 }
 
-data "archive_file" "crud_goal_lambda_layer_zip" {
+data "archive_file" "goal_lambda_layer_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../lambda/crud_goal/layer"
   output_path = "${path.module}/../lambda/crud_goal/lambda-layer.zip"
-  
-  depends_on = [null_resource.crud_goal_lambda_layer]
+
+  depends_on = [null_resource.goal_lambda_layer]
 }
 
-resource "aws_lambda_layer_version" "crud_goal_python_packages_layer" {
-  filename            = data.archive_file.crud_goal_lambda_layer_zip.output_path
-  layer_name          = "${var.project_name}-crud-goal-python-packages-layer"
+resource "aws_lambda_layer_version" "goal_python_packages_layer" {
+  filename            = data.archive_file.goal_lambda_layer_zip.output_path
+  layer_name          = "${var.project_name}-goal-python-packages-layer"
   compatible_runtimes = ["python3.9"]
-  source_code_hash    = data.archive_file.crud_goal_lambda_layer_zip.output_base64sha256
+  source_code_hash    = data.archive_file.goal_lambda_layer_zip.output_base64sha256
+  description         = "Python packages layer for goal Lambda"
 
-  description = "Python packages layer for crud_goal function"
-  
-  depends_on = [null_resource.crud_goal_lambda_layer]
+  depends_on = [null_resource.goal_lambda_layer]
 }
 
-# Null resource to control lambda updates based on source file changes
-resource "null_resource" "crud_goal_lambda_source_hash" {
+# --- Lambda Source Hash ---
+resource "null_resource" "goal_lambda_source_hash" {
   triggers = {
     source_code = filemd5("${path.module}/../lambda/crud_goal/index.py")
   }
 }
 
-# Lambda function code archive
-data "archive_file" "crud_goal_lambda_zip" {
+# --- Lambda Zip ---
+data "archive_file" "goal_lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../lambda/crud_goal"
   output_path = "${path.module}/../lambda/crud_goal/lambda.zip"
   excludes    = ["requirements.txt", "layer/"]
 }
 
-# Lambda function
+# --- Lambda Function ---
 resource "aws_lambda_function" "crud_goal" {
-  filename         = data.archive_file.crud_goal_lambda_zip.output_path
-  function_name    = "${var.project_name}-crud-goal"
-  role            = aws_iam_role.lambda_execution.arn
-  handler         = "index.handler"
-  runtime         = "python3.9"
-  timeout         = 30
-  source_code_hash = null_resource.crud_goal_lambda_source_hash.triggers.source_code
-  layers          = [aws_lambda_layer_version.crud_goal_python_packages_layer.arn]
+  filename         = data.archive_file.goal_lambda_zip.output_path
+  function_name    = "${var.project_name}-goal-handler"
+  role             = aws_iam_role.lambda_execution.arn
+  handler          = "index.lambda_handler"
+  runtime          = "python3.9"
+  timeout          = 30
+  source_code_hash = null_resource.goal_lambda_source_hash.triggers.source_code
+  layers           = [aws_lambda_layer_version.goal_python_packages_layer.arn]
 
   vpc_config {
     subnet_ids         = aws_subnet.private[*].id
@@ -74,6 +73,6 @@ resource "aws_lambda_function" "crud_goal" {
   }
 
   tags = {
-    Name = "${var.project_name}-crud-goal"
+    Name = "${var.project_name}-goal-handler"
   }
 }
